7a34e169193a68465fac19f45af304e5c8828af2
jvincent <jvincent@iastate.edu>
2017-10-22 01:01:45 +0000

    started working on 311 project1 finally

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CUR = COMS331
+CUR = COMS311
 
 COMS321_DIR = ./coms321/hw7/
 COMS321_FILE = hw7
@@ -8,19 +8,27 @@ COMS331_DIR = ./coms331/hw8/
 COMS331_FILE = hw8
 COMS331_MODE = pdf
 
-COMS311_DIR = ./coms311/hw3/
-COMS311_FILE = jvincent-HW3
-COMS311_MODE = pdf
+COMS311_DIR = ./coms311/project1/
+COMS311_FILE = $(addprefix bin/, BinaryST WarWithArray WarWithBST WarWithHash WarWithRollHash)
+COMS311_MODE = class
 
 ENG314_DIR = ./engl314/instructional/
 ENG314_FILE = plane
 ENG314_MODE = pdf
 
-default: $($(CUR)_DIR)$($(CUR)_FILE).$($(CUR)_MODE)
-	@echo "made $<"
+default: $(addprefix $($(CUR)_DIR), $(addsuffix .$($(CUR)_MODE), $($(CUR)_FILE)))
+	@echo "made $(CUR)"
 
-%.pdf: $($(CUR)_DIR)$($(CUR)_FILE).tex
-	@pdflatex -interaction=nonstopmode -output-directory $($(CUR)_DIR) $($(CUR)_DIR)$($(CUR)_FILE).tex >> $($(CUR)_DIR)latexgarbage.txt
-	@rm $($(CUR)_DIR)$($(CUR)_FILE).log $($(CUR)_DIR)latexgarbage.txt
+%.pdf: %.tex
+	@pdflatex -interaction=nonstopmode -output-directory $($(CUR)_DIR) $< >> $($(CUR)_DIR)latexgarbage.txt
+	@rm $($(CUR)_DIR)*.log $($(CUR)_DIR)latexgarbage.txt
 
-.PHONY: default
+$($(CUR)_DIR)bin/%.class: makebin $($(CUR)_DIR)%.java
+	@javac -Werror -d $($(CUR)_DIR)/bin/ -cp $($(CUR)_DIR)/bin:$($(CUR)_DIR) -Xlint $(word 2,$^)
+
+makebin:
+	@[  -d $($(CUR)_DIR)/bin ] || echo "making bin folder"
+	@[  -d $($(CUR)_DIR)/bin ] || mkdir $($(CUR)_DIR)/bin
+
+
+.PHONY: default makebin
--- coms311/project1/BinaryST.java
+++ coms311/project1/BinaryST.java
@@ -11,64 +11,225 @@
 public class BinaryST
 {
 	// member fields and methods
-	
+	private Node root;
+
 	public BinaryST()
 	{
-		// implementation
+		this.root = null;
 	}
-	
+
 	public BinaryST(String[] s)
 	{
-		// implementation
+    this.root = null;
+		for(int i = 0; i < s.length; s++){
+      this.add(s[i]);
+    }
 	}
-	
+
 	public int distinctSize()
 	{
-		// implementation
+		return this.root.uniqueChildren + 1;
 	}
-	
+
 	public int size()
 	{
-		// implementation
+		return this.root.children + this.root.count;
 	}
-	
+
 	public int height()
 	{
-		// implementation
+		return this.root.height
 	}
-	
+
 	public void add(String s)
 	{
-		// implementation
+		this.root.insert(s);
 	}
-	
+
 	public boolean search(String s)
 	{
-		// implementation
+		return this.root.search(s) != null;
 	}
-	
+
 	public int frequency(String s)
 	{
-		// implementation
+		Node ans = this.root.search(s);
+    if(ans == null){
+      return 0;
+    } else{
+      return ans.count;
+    }
 	}
-	
+
 	public boolean remove(String s)
 	{
-		// implementation
+    return this.root.remove(s)[0];
 	}
-	
+
 	public String[] inOrder()
 	{
 		// implementation
 	}
-	
+
 	public String[] preOrder()
 	{
 		// implementation
 	}
-	
+
 	public int rankOf(String s)
 	{
 		// implementation
 	}
+
+  private class Node implements comparable{
+
+    public final String value;
+
+    public Node lChild = null;
+
+    public Node rChild = null;
+
+    public Node Parent = null;
+
+    public int height = 1;
+
+    public int count = 1;
+
+    public int children = 0;
+
+    public int uniqueChildren = 0;
+
+    public Node(String value){
+      this.value = value;
+    }
+
+    public Node(String value, Node parent){
+      this.value = value;
+      this.parent = parent;
+    }
+
+    public boolean insert(String s){
+      int compare = this.compareTo(s);
+      boolean result;
+
+      if(compare > 0){
+        if(this.lChild == null){
+          result = true;
+          this.lChild = new Node(s);
+          this.increment(true);
+        } else{
+          result = this.lChild.insert(s);
+          this.increment(result);
+        }
+      } else if(compare < 0){
+        if(this.rChild == null){
+          result = true;
+          this.rChild = new Node(s);
+          this.increment(true);
+        } else{
+          result = this.rChild.insert(s);
+          this.increment(result);
+        }
+      } else {
+        result = false;
+        count++;
+      }
+
+      return result;
+    }
+
+    public Node search(String s){
+      int compare = this.compareTo(s);
+
+      if(compare > 0){
+        if(this.lChild == null){
+          return null;
+        }else{
+          return this.lChild.search(s);
+        }
+      } else if(compare < 0){
+        if(this.rChild == null){
+          return null;
+        }else{
+          return this.rChild.search(s);
+        }
+      } else{
+        return this;
+      }
+    }
+
+    public boolean[] remove(String s){
+      boolean[] ans = new boolean[2];
+      int compare = this.compareTo(s);
+
+      if(compare < 0){
+        if(this.rChild == null){
+          ans[0] = false;
+        } else{
+          ans = this.rChild.remove(s);
+        }
+      } else if(compare > 0){
+        if(this.lChild == null){
+          ans[0] = false;
+        } else{
+          ans = this.lChild.remove(s);
+        }
+      } else{
+        ans[0] = true;
+        if(this.count > 1){
+          count--;
+          ans[1] = false;
+        } else{
+          ans[1] = true;
+          Node replacement = this.findReplacement(true);
+          replacement
+        }
+      }
+
+      return ans;
+    }
+
+    /**
+     * returns a detached node that can be inserted into the place of the node
+     * this method was called on. This method also takes care of decrementing the children and unique children fields of the
+     * nodes it passes over while finding the replacement.
+     * @param  boolean                 first true if this is the base element that the method was called on
+     * @return Node                    A Node that is a child of the root node that has been detached from the graph. null if there are no children.
+     * @author Collin Vincent collinvincent96@gmail.com
+     * @date   2017-10-22T00:50:41+000
+     */
+    private Node findReplacement(boolean first){
+      if(this.lChild != null && this.rChild != null){
+
+      } else if(this.lChild != null){
+
+      } else if(this.rChild != null){
+
+      } else{
+        return null;
+      }
+    }
+
+    private void increment(boolean unique){
+      this.children++
+      if(unique){
+        this.uniqueChildren++
+        this.calcHeight();
+      }
+    }
+
+    private void calcHeight(){
+      int lHeight = this.lChild == null ? 0 : this.lChild.height;
+      int rHeight = this.rChild == null ? 0 : this.rChild.height;
+      this.height = lHeight < rHeight ? rHeight + 1 : lHeight + 1;
+    }
+
+    public int compareTo(String other){
+      return this.value.compareTo(other);
+    }
+
+    public int compareTo(Node other){
+      return this.value.compareTo(other.value);
+    }
+  }
 }
